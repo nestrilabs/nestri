75ea50627a5137c7a0e32de4282899fb159b701e
Wanjohi <elviswanjohi47@gmail.com>
2024-12-24 18:33:57 +0300

    feat: Add tags/\labels for the games section

--- apps/www/src/routes/index.tsx
+++ apps/www/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 import { HeroSection, Cursor, MotionComponent, transition } from "@nestri/ui/react"
 import { NavBar, Footer, Modal, Card } from "@nestri/ui"
+import { cn } from "@nestri/ui/design";
 
 const features = [
   {
@@ -52,51 +53,40 @@ const features = [
   }
 ]
 
-const games = [
+const tags = [
   {
-    title: "Apex Legends",
-    rotate: -5,
-    titleWidth: 31.65,
-    titleHeight: 82.87,
-    id: 1172470,
+    name: "All",
+    total: undefined
   },
   {
-    title: "Control Ultimate Edition",
-    rotate: 3,
-    titleWidth: 55.61,
-    titleHeight: 100,
-    id: 870780,
+    name: "Top Picks",
+    total: 45
   },
   {
-    title: "Black Myth: Wukong",
-    rotate: -3,
-    titleWidth: 56.30,
-    titleHeight: 69.79,
-    id: 2358720,
+    name: "Action",
+    total: 47
   },
   {
-    title: "Shell Runner - Prelude",
-    rotate: 2,
-    id: 2581970,
-    titleWidth: 72.64,
-    titleHeight: 91.26,
+    name: "Free To Play",
+    total: 53
   },
   {
-    title: "Counter-Strike 2",
-    rotate: -5,
-    id: 730,
-    titleWidth: 50.51,
-    titleHeight: 99.65,
+    name: "Adventure",
+    total: 21
   },
   {
-    title: "Add from Steam",
-    rotate: 7,
+    name: "Casual",
+    total: 26
+  },
+  {
+    name: "Indie",
+    total: 74
   }
 ]
 
 export default component$(() => {
   return (
-    <>
+    <div class="w-screen relative">
       <NavBar />
       <HeroSection client:load>
         <div class="w-full flex flex-col">
@@ -115,9 +105,18 @@ export default component$(() => {
           </p>
         </div>
       </HeroSection>
-      <div class="items-center justify-center w-full flex">
-        <section class="relative py-10 flex-col w-full overflow-hidden">
-          
+      <div class="items-center justify-center w-full flex py-8 px-4">
+        <section class="sticky w-full max-w-4xl px-1 mx-auto py-3 md:overflow-hidden overflow-x-scroll flex gap-1.5">
+          <button class="bg-transparent text-gray-900/70 focus:ring-primary-500 outline-none dark:text-gray-100/70 ring-2 mt-[1px] ring-gray-300 dark:ring-gray-700 w-48 h-max py-2 rounded-full flex text-sm px-4 items-center gap-1 hover:bg-gray-100 dark:hover:bg-gray-900 transition-all duration-200">
+            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24" class="size-[14px] -mt-[1px] flex-shrink-0"><g fill="none" stroke="currentColor" stroke-width="2"><circle cx="11.5" cy="11.5" r="9.5"></circle><path stroke-linecap="round" d="M18.5 18.5L22 22"></path></g></svg>
+            Search...
+          </button>
+          {tags.map((tag, key) => (
+            <button key={`tags-${key}`} class={cn("bg-white dark:bg-black text-gray-900/70 hover:ring-primary-500 outline-none dark:text-gray-100/70 ring-2 text-sm h-max ring-gray-300 py-2 px-4 rounded-full flex items-center hover:bg-gray-100 dark:hover:bg-gray-900 transition-all duration-200 focus:bg-primary-100 focus:text-primary-500 focus:ring-primary-500 focus:!bg-[url:none]", key == 1 && "bg-[url:linear-gradient(135deg,theme(colors.gray.100),theme(colors.gray.300)_20%,theme(colors.gray.100)_40%,theme(colors.gray.300)_60%,theme(colors.gray.100)_80%,theme(colors.gray.300))]")}>
+              <p class="whitespace-nowrap"> {tag.name}</p>
+              {tag.total && <sup class="pl-1 font-title" >{tag.total}</sup>}
+            </button>
+          ))}
 
         </section>
       </div>
@@ -700,7 +699,7 @@ export default component$(() => {
         </MotionComponent>
       </section> */}
       <Footer />
-    </>
+    </div>
   );
 });
 
