c39866a0194299840c3811ce4b0907ebd555caa3
Wanjohi <elviswanjohi47@gmail.com>
2024-12-26 22:46:13 +0300

    🐛 fix: Fix a typo in pricing page

--- apps/www/src/routes/pricing/index.tsx
+++ apps/www/src/routes/pricing/index.tsx
@@ -159,7 +159,7 @@ export default component$(() => {
                                                         <path fill-rule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clip-rule="evenodd"></path>
                                                     </svg>
                                                 </div>
-                                                <p>Unlimited cloud saves</p>
+                                                <p>Unlimited cloud saves <sup>1</sup></p>
                                             </div>
                                         </div>
                                         <div class="gap-2.5 flex relative items-center w-full" >
