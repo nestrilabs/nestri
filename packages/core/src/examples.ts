37c442dc92c1f88578548451efbdf5d9a58c2d85
Wanjohi <elviswanjohi47@gmail.com>
2025-01-04 16:06:20 +0300

    fix: typo

--- packages/core/src/examples.ts
+++ packages/core/src/examples.ts
@@ -7,7 +7,7 @@ export module Examples {
 
     export const Machine = {
         id: "0bfcb712-df13-4454-81a8-fbee66eddca4",
-        hostname: "desktopeuo8vsf",
+        hostname: "DESKTOP-EUO8VSF",
         fingerprint: "fc27f428f9ca47d4b41b70889ae0c62090",
         createdAt: '2025-01-04T11:56:23.902Z'
     }
