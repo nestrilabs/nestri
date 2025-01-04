1a46ead886c2ec96c716fe0a47d9bacf3c8f03d0
Wanjohi <elviswanjohi47@gmail.com>
2025-01-05 00:50:10 +0300

    fix

--- packages/core/src/database.ts
+++ packages/core/src/database.ts
@@ -2,16 +2,11 @@ import { Resource } from "sst";
 import { init } from "@instantdb/admin";
 import schema from "../instant.schema";
 
-const databaseClient = () => {
-    console.log("appid", Resource.InstantAppId.value)
-    console.log("token", Resource.InstantAdminToken.value)
-
-    return init({
+const databaseClient = () => init({
         appId: Resource.InstantAppId.value,
         adminToken: Resource.InstantAdminToken.value,
         schema
     })
-}
 
 
 export default databaseClient
