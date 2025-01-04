dede878c3cd942d22faeee57bf44f54d0be768bb
Wanjohi <elviswanjohi47@gmail.com>
2025-01-04 23:19:42 +0300

    🐜 fix(www): Fix a typo

--- apps/www/src/routes/pricing/index.tsx
+++ apps/www/src/routes/pricing/index.tsx
@@ -193,7 +193,7 @@ export default component$(() => {
                                                 <div class="size-5 relative">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-full h-full" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="m17 7.83l1.697 1.526c1.542 1.389 2.313 2.083 2.313 2.974c0 .89-.771 1.585-2.314 2.973L17 16.83M13.987 5L12 12.415l-1.987 7.415M7 7.83L5.304 9.356C3.76 10.745 2.99 11.44 2.99 12.33s.771 1.585 2.314 2.973L7 16.83" /></svg>
                                                 </div>
-                                                <p>No API access </p>
+                                                <p>Limited API access </p>
                                             </div>
                                         </div>
 
