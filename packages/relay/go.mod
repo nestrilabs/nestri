fc8be2d2bcd74859e4d784c930aede7902c2cc64
Wanjohi <elviswanjohi47@gmail.com>
2025-01-12 06:20:02 +0300

    ⭐feat(relay): Add retransmission

--- packages/relay/go.mod
+++ packages/relay/go.mod
@@ -10,6 +10,15 @@ require (
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pion/datachannel v1.5.9 // indirect
 	github.com/pion/dtls/v3 v3.0.4 // indirect
 	github.com/pion/ice/v4 v4.0.2 // indirect
@@ -24,8 +33,10 @@ require (
 	github.com/pion/stun/v3 v3.0.0 // indirect
 	github.com/pion/transport/v3 v3.0.7 // indirect
 	github.com/pion/turn/v4 v4.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/wlynxg/anet v0.0.5 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
--- packages/relay/main.go
+++ packages/relay/main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/signal"
+	"fmt"
 	relay "relay/internal"
-	"syscall"
 )
 
 func main() {
-	var err error
-	stopCh := make(chan os.Signal, 1)
-	signal.Notify(stopCh, os.Interrupt, syscall.SIGTERM)
+	// var err error
+	// stopCh := make(chan os.Signal, 1)
+	// signal.Notify(stopCh, os.Interrupt, syscall.SIGTERM)
 
-	// Get flags and log them
-	relay.InitFlags()
-	relay.GetFlags().DebugLog()
+	// // Get flags and log them
+	// relay.InitFlags()
+	// relay.GetFlags().DebugLog()
 
-	// Init WebRTC API
-	err = relay.InitWebRTCAPI()
+	// // Init WebRTC API
+	// err = relay.InitWebRTCAPI()
+	// if err != nil {
+	// 	log.Fatal("Failed to initialize WebRTC API: ", err)
+	// }
+
+	// // Start our HTTP endpoints
+	// relay.InitHTTPEndpoint()
+
+	// // Wait for exit signal
+	// <-stopCh
+	// log.Println("Shutting down gracefully by signal...")
+	party, err := relay.NewParty("party-1", "localhost", nil)
 	if err != nil {
-		log.Fatal("Failed to initialize WebRTC API: ", err)
+		fmt.Println("Error running the party", err)
 	}
 
-	// Start our HTTP endpoints
-	relay.InitHTTPEndpoint()
-
-	// Wait for exit signal
-	<-stopCh
-	log.Println("Shutting down gracefully by signal...")
+	party.Connect()
 }
