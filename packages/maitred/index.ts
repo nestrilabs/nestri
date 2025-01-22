f6eff1346c33fc5f5f13a37aa7abfc79df94eeb4
Wanjohi <elviswanjohi47@gmail.com>
2025-01-23 00:21:15 +0300

    feat: Add maitred

---
+++ packages/maitred/index.ts
@@ -0,0 +1,26 @@
+import { color } from "bun" with { type: "macro" };
+
+const infoBgColor = color("#f267ad", "ansi")?.replace("38", "48")
+const errorBgColor = color("red", "ansi")?.replace("38", "48")
+const resetColor = "\x1b[0m"// color("transparent", "ansi")?.replace("38", "48")
+
+
+const log = {
+    Info: (r: string) => console.log(`> ${infoBgColor}[INF]${resetColor} ${r}`),
+    Error: (r: string) => console.log(`> ${errorBgColor}[ERR]${resetColor} ${r}`)
+}
+
+const handler = () => {
+    const teamID = process.argv[2];
+
+    if (!teamID) {
+        log.Error("Please provide the team ID to register this container to")
+        process.exit(1)
+    }
+
+    log.Info("Started successfully")
+    log.Error("Did not start successfully")
+}
+
+handler()
+
