39fff1896aba8d88cee2623bc206216c8be2efdf
Wanjohi <elviswanjohi47@gmail.com>
2025-01-26 09:13:28 +0300

    feat: Add cluster

--- infra/cluster.ts
+++ infra/cluster.ts
@@ -4,53 +4,50 @@
 //     name: "NestriHostedCluster"
 // });
 
-// const vpc = new sst.aws.Vpc("MyVpc");
-
-const securityGroup = new aws.ec2.SecurityGroup("WebSecurityGrp", {
-    ingress: [
-        {
-            protocol: "tcp",
-            fromPort: 80,
-            toPort: 80,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-    ],
-});
+// const securityGroup = new aws.ec2.SecurityGroup("WebSecurityGrp", {
+//     ingress: [
+//         {
+//             protocol: "tcp",
+//             fromPort: 80,
+//             toPort: 80,
+//             cidrBlocks: ["0.0.0.0/0"],
+//         },
+//     ],
+// });
 
 // Find the latest Ubuntu AMI
-const ami = aws.ec2.getAmi({
-    filters: [
-        {
-            name: "name",
-            values: ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"],
-        },
-    ],
-    mostRecent: true,
-    owners: ["099720109477"], // Canonical
-});
+// const ami = aws.ec2.getAmi({
+//     filters: [
+//         {
+//             name: "name",
+//?Note: Bottlerocket does not support encrypted EFS instances or ones with 'awsvpc'
+//             values: ["bottlerocket-aws-ecs-2-nvidia-x86_64-*"],
+//         },
+//     ],
+//     mostRecent: true,
+//     owners: ["092701018921"],
+// });
 
-const server = new aws.ec2.Instance("Ec2", {
-    instanceType: "t2.micro",
+const server = new aws.ec2.Instance("NestriGPU", {
+    instanceType: aws.ec2.InstanceType.G4dn_XLarge,
     ami: ami.then((ami) => ami.id),
-    userData: $interpolate`#!/bin/bash
-    echo "Hello, World!" > index.html
-    nohup python3 -m http.server 80 &`,
-    vpcSecurityGroupIds: [securityGroup.id],
+    userData: $interpolate`
+[settings.ecs]
+cluster = "${cluster.name}"
+reserved-memory=300
+container-stop-timeout="3h"
+`,
+    // vpcSecurityGroupIds: [securityGroup.id],
+    instanceMarketOptions: {
+        marketType: "spot",
+        spotOptions: {
+            maxPrice: "0.0031",
+        },
+    },
     associatePublicIpAddress: true,
 });
 
-export const outputs = {
-    hostedCluster: server.publicIp
-}
-
-// const launchTemplate = new aws.ec2.LaunchTemplate("Ec2LaunchTemplate", {
-//     name: "myLaunchTemplate",
-//     instanceType: "t2.micro",//"g4dn.xlarge", // g4dn instance type
-//     imageId: ami.then((ami) => ami.id),
-//     networkInterfaces: [{
-//         associatePublicIpAddress: "true",
-//         securityGroups: ["sg-0c508d8f0e5e1a5c5"], // Replace with your security group ID
-//     }],
-//     userData: $interpolate`#!/bin/bash
-// echo ECS_CLUSTER=${ecsCluster.name} >> /etc/ecs/ecs.config`
-// });
+// export const outputs = {
+//     // hostedCluster: server.publicIp
+//     // ami: ami.then((ami) => ami.id)
+// }
