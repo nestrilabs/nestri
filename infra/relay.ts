6031d52ee1eeb3b2da9c0519d144385ce1d7b884
Wanjohi <elviswanjohi47@gmail.com>
2025-01-28 20:16:10 +0300

    feat: FYI

--- infra/relay.ts
+++ infra/relay.ts
@@ -3,22 +3,22 @@ import { execSync } from "child_process";
 
 // const vpc =  sst.aws.createTaskDefinition("Task",{})
 
-new aws.ecs.Service('service', {
-    name: 'awsfundamentals',
-    cluster: cluster.arn,
-    desiredCount: 1,
-    launchType: 'FARGATE',
-    taskDefinition: taskDefinition.arn,
-    networkConfiguration: {
-      assignPublicIp: true,
-      subnets: subnets.map((s) => s.id),
-      securityGroups: [securityGroup.id],
-    },
-    loadBalancers: [
-      {
-        // targetGroupArn: targetGroup.arn,
-        containerName: 'backend',
-        containerPort: 80,
-      },
-    ],
-  });
+// new aws.ecs.Service('service', {
+//     name: 'awsfundamentals',
+//     cluster: cluster.arn,
+//     desiredCount: 1,
+//     launchType: 'FARGATE',
+//     taskDefinition: taskDefinition.arn,
+//     networkConfiguration: {
+//       assignPublicIp: true,
+//       subnets: subnets.map((s) => s.id),
+//       securityGroups: [securityGroup.id],
+//     },
+//     loadBalancers: [
+//       {
+//         // targetGroupArn: targetGroup.arn,
+//         containerName: 'backend',
+//         containerPort: 80,
+//       },
+//     ],
+//   });
