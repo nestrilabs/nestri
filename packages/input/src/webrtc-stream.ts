c2363b0bceb7bc5cce6d2ef218ae98df2e1cb82e
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2025-01-29 04:16:27 +0300

    ⭐ feat: Add protobuf (#171)
    This is a second attempt to add protobuf to Nestri, after the first one
    failed
    
    ---------
    
    Co-authored-by: Philipp Neumann <3daquawolf@gmail.com>
    Co-authored-by: DatCaptainHorse <DatCaptainHorse@users.noreply.github.com>

--- packages/input/src/webrtc-stream.ts
+++ packages/input/src/webrtc-stream.ts
@@ -6,8 +6,6 @@ import {
   MessageAnswer,
   JoinerType,
   AnswerType,
-  decodeMessage,
-  encodeMessage
 } from "./messages";
 
 export class WebRTCStream {
@@ -40,16 +38,16 @@ export class WebRTCStream {
         payload_type: "join",
         joiner_type: JoinerType.JoinerClient
       };
-      this._ws!.send(encodeMessage(joinMessage));
+      this._ws!.send(JSON.stringify(joinMessage));
     }
 
     let iceHolder: RTCIceCandidateInit[] = [];
 
     this._ws.onmessage = async (e) => {
-      // allow only binary
-      if (typeof e.data !== "object") return;
+      // allow only JSON
+      if (typeof e.data === "object") return;
       if (!e.data) return;
-      const message = await decodeMessage<MessageBase>(e.data);
+      const message = JSON.parse(e.data) as MessageBase;
       switch (message.payload_type) {
         case "sdp":
           if (!this._pc) {
@@ -63,7 +61,7 @@ export class WebRTCStream {
           // Force stereo in Chromium browsers
           answer.sdp = this.forceOpusStereo(answer.sdp!);
           await this._pc!.setLocalDescription(answer);
-          this._ws!.send(encodeMessage({
+          this._ws!.send(JSON.stringify({
             payload_type: "sdp",
             sdp: answer
           }));
@@ -154,7 +152,7 @@ export class WebRTCStream {
           payload_type: "ice",
           candidate: e.candidate
         };
-        this._ws!.send(encodeMessage(message));
+        this._ws!.send(JSON.stringify(message));
       }
     }
 
