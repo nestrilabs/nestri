30eb6c7d57eb82198a2f67141ac8d5188688e19e
Wanjohi <elviswanjohi47@gmail.com>
2025-01-31 10:53:30 +0300

    fix: Add some more

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,2 +1,12 @@
-const storage = new sst.aws.Efs("HostedStorage")
+// const storage = new sst.aws.Efs("HostedStorage")
+
+const vpc = new sst.aws.Vpc("StorageVpc", { az: 2 })
+
+
+const filesystem = new aws.efs.FileSystem("HostedStorage",{
+    performanceMode: "generalPurpose",
+    throughputMode: "elastic",
+    encrypted: true,
+})
+
 
