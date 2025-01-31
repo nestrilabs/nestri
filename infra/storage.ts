4e01be6713898d234d0f01b453542603a534d591
Wanjohi <elviswanjohi47@gmail.com>
2025-01-31 11:29:45 +0300

    feat: Add mount targets

--- infra/storage.ts
+++ infra/storage.ts
@@ -3,10 +3,43 @@
 const vpc = new sst.aws.Vpc("StorageVpc", { az: 2 })
 
 
-const filesystem = new aws.efs.FileSystem("HostedStorage",{
+const fileSystem = new aws.efs.FileSystem("HostedStorage", {
     performanceMode: "generalPurpose",
     throughputMode: "elastic",
     encrypted: true,
 })
 
+const securityGroup = new aws.ec2.SecurityGroup(
+    `NestriHostedSecGroup`,
+    {
+        description: "Managed by SST",
+        vpcId: vpc.id,
+        egress: [
+            {
+                fromPort: 0,
+                toPort: 0,
+                protocol: "-1",
+                cidrBlocks: ["0.0.0.0/0"],
+            },
+        ],
+        ingress: [
+            {
+                fromPort: 0,
+                toPort: 0,
+                protocol: "-1",
+                // Restricts inbound traffic to only within the VPC
+                cidrBlocks: [vpc.nodes.vpc.cidrBlock],
+            },
+        ],
+    }
+);
 
+vpc.privateSubnets.apply((subnets) =>
+    subnets.map(
+        (subnet) =>
+            new aws.efs.MountTarget(`NestriMountTarget${subnet}`, {
+                fileSystemId: fileSystem.id,
+                subnetId: subnet,
+                securityGroups: [securityGroup.id],
+            })
+    ))
