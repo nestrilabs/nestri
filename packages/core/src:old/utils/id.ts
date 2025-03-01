178c612f0f5d9bcf21879e37db0a067b876c3d8f
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2025-03-02 00:01:25 +0300

    🐜 fix: Make sure the db uses the same name (#199)
    ## Description
    This fixes the issue where Cloudflare fails
    
    ## Related Issues
    <!-- List any related issues (e.g., "Closes #123", "Fixes #456") -->
    
    ## Type of Change
    
    - [x] Bug fix (non-breaking change)
    - [ ] New feature (non-breaking change)
    - [ ] Breaking change (fix or feature that changes existing
    functionality)
    - [ ] Documentation update
    - [ ] Other (please describe):
    
    ## Checklist
    
    - [x] I have updated relevant documentation
    - [x] My code follows the project's coding style
    - [x] My changes generate no new warnings/errors

--- packages/core/src:old/utils/id.ts
+++
@@ -1,9 +0,0 @@
-import { ulid } from "ulid";
-
-export const prefixes = {
-  user: "usr",
-} as const;
-
-export function createID(prefix: keyof typeof prefixes): string {
-  return [prefixes[prefix], ulid()].join("_");
-}
