57a05ddaa1779ce9ee6b216dafb874aa34bd7dea
Wanjohi <elviswanjohi47@gmail.com>
2025-03-01 23:22:05 +0300

    🐜 fix: Make sure the db uses the same name

--- infra:old/dns.ts
+++
@@ -1,9 +0,0 @@
-export const domain =
-  {
-    production: "nestri.io",
-    dev: "dev.nestri.io",
-  }[$app.stage] || $app.stage + ".dev.nestri.io";
-
-  export const zone = cloudflare.getZoneOutput({
-    name: "nestri.io",
-  });
