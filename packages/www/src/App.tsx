725e494deabf2fd39dccb78e5bd1357241b30064
Wanjohi <elviswanjohi47@gmail.com>
2025-03-13 05:48:45 +0300

    fix: Fix authentication issues

--- packages/www/src/App.tsx
+++ packages/www/src/App.tsx
@@ -12,8 +12,9 @@ import { Navigate, Route, Router } from "@solidjs/router";
 import { globalStyle, macaron$ } from "@macaron-css/core";
 import { useStorage } from '@nestri/www/providers/account';
 import { CreateTeamComponent } from '@nestri/www/pages/new';
-import { AuthProvider, useAuth } from '@nestri/www/providers/auth';
+import { OpenAuthProvider, useOpenAuth } from "@openauthjs/solid";
 import { darkClass, lightClass, theme } from '@nestri/www/ui/theme';
+import { AccountProvider, useAccount } from '@nestri/www/providers/account';
 import { Component, createSignal, Match, onCleanup, Switch } from 'solid-js';
 
 const Root = styled("div", {
@@ -83,45 +84,51 @@ export const App: Component = () => {
     const storage = useStorage();
 
     return (
-        <Root class={theme() === "light" ? lightClass : darkClass} id="styled">
-            <Router>
-                <Route
-                    path="*"
-                    component={(props) => (
-                        <AuthProvider>
-                            {props.children}
-                        </AuthProvider>
-                        // props.children
-                    )}
-                >
-                    <Route path=":teamSlug">{TeamRoute}</Route>
-                    <Route path="new" component={CreateTeamComponent} />
+        <OpenAuthProvider
+            issuer={import.meta.env.VITE_AUTH_URL}
+            clientID="web"
+        >
+            <Root class={theme() === "light" ? lightClass : darkClass} id="styled">
+                <Router>
                     <Route
-                        path="/"
-                        component={() => {
-                            const auth = useAuth();
-                            return (
-                                <Switch>
-                                    <Match when={auth.current.teams.length > 0}>
-                                        <Navigate
-                                            href={`/${(
-                                                auth.current.teams.find(
-                                                    (w) => w.id === storage.value.team,
-                                                ) || auth.current.teams[0]
-                                            ).slug
-                                                }`}
-                                        />
-                                    </Match>
-                                    <Match when={true}>
-                                        <Navigate href={`/new`} />
-                                    </Match>
-                                </Switch>
-                            );
-                        }}
-                    />
-                    {/* <Route path="*" component={() => <NotFound />} /> */}
-                </Route>
-            </Router>
-        </Root>
+                        path="*"
+                        component={(props) => (
+                            <AccountProvider>
+                                {props.children}
+                            </AccountProvider>
+                            // props.children
+                        )}
+                    >
+                        <Route path=":teamSlug">{TeamRoute}</Route>
+                        <Route path="new" component={CreateTeamComponent} />
+                        <Route
+                            path="/"
+                            component={() => {
+                                const auth = useOpenAuth();
+                                const account = useAccount();
+                                return (
+                                    <Switch>
+                                        <Match when={account.current.teams.length > 0}>
+                                            <Navigate
+                                                href={`/${(
+                                                    account.current.teams.find(
+                                                        (w) => w.id === storage.value.team,
+                                                    ) || account.current.teams[0]
+                                                ).slug
+                                                    }`}
+                                            />
+                                        </Match>
+                                        <Match when={true}>
+                                            <Navigate href={`/new`} />
+                                        </Match>
+                                    </Switch>
+                                );
+                            }}
+                        />
+                        {/* <Route path="*" component={() => <NotFound />} /> */}
+                    </Route>
+                </Router>
+            </Root>
+        </OpenAuthProvider>
     )
 }
--- packages/www/src/providers/account.tsx
+++ packages/www/src/providers/account.tsx
@@ -1,4 +1,4 @@
-import { createStore } from "solid-js/store";
+import { createStore, reconcile } from "solid-js/store";
 import { makePersisted } from "@solid-primitives/storage";
 import { ParentProps, createContext, useContext } from "solid-js";
 
@@ -10,7 +10,6 @@ function init() {
     createStore({
       account: "",
       team: "",
-      dummy: "",
     })
   );
 
@@ -32,3 +31,73 @@ export function useStorage() {
   }
   return ctx;
 }
+
+import { createEffect } from "solid-js";
+import { useOpenAuth } from "@openauthjs/solid"
+import { Team } from "@nestri/core/team/index";
+import { createInitializedContext } from "../common/context";
+
+type Storage = {
+  accounts: Record<string, {
+    id: string
+    email: string
+    avatarUrl: string
+    discriminator: number
+    name: string;
+    polarCustomerID: string;
+    teams: Team.Info[];
+  }>
+}
+
+export const { use: useAccount, provider: AccountProvider } = createInitializedContext("AccountContext", () => {
+  const auth = useOpenAuth()
+  const [store, setStore] = makePersisted(
+    createStore<Storage>({
+      accounts: {},
+    }),
+    {
+      name: "nestri.account",
+    },
+  );
+
+  async function refresh(id: string) {
+    const access = await auth.access(id).catch(() => { })
+    if (!access) {
+      auth.authorize()
+      return
+    }
+    return await fetch(import.meta.env.VITE_API_URL + "/account", {
+      headers: {
+        authorization: `Bearer ${access}`,
+      },
+    })
+      .then(val => val.json())
+      .then(val => setStore("accounts", id, reconcile(val.data)))
+  }
+
+  createEffect((previous: string[]) => {
+    if (!Object.values(auth.all).length) {
+      auth.authorize()
+      return []
+    }
+    for (const item of Object.values(auth.all)) {
+      if (previous.includes(item.id)) continue
+      refresh(item.id)
+    }
+    return Object.keys(auth.all)
+  }, [] as string[])
+
+  return {
+    get all() {
+      return store.accounts
+    },
+    get current() {
+      return store.accounts[auth.subject!.id]
+    },
+    refresh,
+    get ready() {
+      if (!auth.subject) return false
+      return store.accounts[auth.subject.id] !== undefined
+    }
+  }
+})
