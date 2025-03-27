81e3193eaed644b9524b098a6f91f6edea053c55
Wanjohi <elviswanjohi47@gmail.com>
2025-03-27 04:40:59 +0300

    ⭐ feat(www): Add logic to the homepage

--- packages/www/src/pages/team/home.tsx
+++ packages/www/src/pages/team/home.tsx
@@ -360,7 +360,7 @@ export function HomeRoute() {
         <>
             <Header whiteColor>
                 <FullScreen >
-                    <EmptyState
+                    {/* <EmptyState
                         style={{
                             "--nestri-qr-dot-color": theme.color.d1000.gray,
                             "--nestri-body-background": theme.color.gray.d100
@@ -384,15 +384,15 @@ export function HomeRoute() {
                         </QRWrapper>
                         <EmptyStateHeader>Sign in to your Steam account</EmptyStateHeader>
                         <EmptyStateSubHeader>Use your Steam Mobile App to sign in via QR code.&nbsp;<SteamMobileLink href="https://store.steampowered.com/mobile" target="_blank">Learn More<svg data-testid="geist-icon" height="20" stroke-linejoin="round" viewBox="0 0 16 16" width="20" style="color: currentcolor;"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.5 9.75V11.25C11.5 11.3881 11.3881 11.5 11.25 11.5H4.75C4.61193 11.5 4.5 11.3881 4.5 11.25L4.5 4.75C4.5 4.61193 4.61193 4.5 4.75 4.5H6.25H7V3H6.25H4.75C3.7835 3 3 3.7835 3 4.75V11.25C3 12.2165 3.7835 13 4.75 13H11.25C12.2165 13 13 12.2165 13 11.25V9.75V9H11.5V9.75ZM8.5 3H9.25H12.2495C12.6637 3 12.9995 3.33579 12.9995 3.75V6.75V7.5H11.4995V6.75V5.56066L8.53033 8.52978L8 9.06011L6.93934 7.99945L7.46967 7.46912L10.4388 4.5H9.25H8.5V3Z" fill="currentColor"></path></svg></SteamMobileLink></EmptyStateSubHeader>
-                    </EmptyState>
-                    {/* <LastPlayedWrapper>
+                    </EmptyState> */}
+                    <LastPlayedWrapper>
                         <LastPlayedFader />
                         <LogoBackgroundImage />
                         <BackgroundImage />
-                        <Material />
-                        <JoeColor />
-                    </LastPlayedWrapper> */}
-                    {/* <GamesContainer>
+                        {/* <Material />
+                        <JoeColor /> */}
+                    </LastPlayedWrapper>
+                    <GamesContainer>
                         <GamesWrapper>
                             <GameSquareImage alt="Assasin's Creed Shadows" src="https://assets-prd.ignimgs.com/2024/05/15/acshadows-1715789601294.jpg" />
                             <GameSquareImage alt="Assasin's Creed Shadows" src="https://assets-prd.ignimgs.com/2022/09/22/slime-rancher-2-button-02-1663890048548.jpg" />
@@ -406,7 +406,7 @@ export function HomeRoute() {
                             <GameImageCapsule alt="Assasin's Creed Shadows" src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/870780/hero_capsule.jpg?t=1737800535" />
                             <GameImageCapsule alt="Assasin's Creed Shadows" src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/2050650/hero_capsule.jpg?t=1737800535" />
                         </SteamLibrary>
-                    </GamesContainer> */}
+                    </GamesContainer>
                 </FullScreen>
             </Header>
         </>
