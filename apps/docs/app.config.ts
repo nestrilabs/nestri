18144643f7dff0387cef6360bcf11b62c834ad0b
Victor Pahuus Petersen <49293748+victorpahuus@users.noreply.github.com>
2025-03-27 20:14:48 +0100

    Revert "Rename image to see if build passes"
    This reverts commit 7a1bcd85e23c1e651868dc45723fda9113b78ee3.

--- apps/docs/app.config.ts
+++ apps/docs/app.config.ts
@@ -14,7 +14,7 @@ export default defineAppConfig({
       showTitle: true,
       darkModeToggle: true,
       logo: {
-        light: '/logo.png',
+        light: '/logo.webp',
         dark: '/logo-dark.svg',
       },
       nav: [{
