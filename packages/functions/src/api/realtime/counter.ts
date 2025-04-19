e4fd26b59ad6094c0a313c3ac4fcbd68eca964d8
Wanjohi <elviswanjohi47@gmail.com>
2025-04-19 23:00:32 +0300

    fix: Fix actor-core by using a global package

---
+++ packages/functions/src/api/realtime/counter.ts
@@ -0,0 +1,15 @@
+import { actor } from "actor-core";
+
+export const counter = actor({
+	state: { count: 0 },
+	actions: {
+		increment: (c, x: number) => {
+			c.state.count += x;
+			c.broadcast("newCount", c.state.count);
+			return c.state.count;
+		},
+		getCount: (c) => {
+			return c.state.count;
+		},
+	},
+});
--- packages/www/src/pages/steam.tsx
+++ packages/www/src/pages/steam.tsx
@@ -1,3 +1,13 @@
-export function SteamComponent() {
+import { useRealtime } from "../providers/realtime"
 
+export async function SteamComponent() {
+    const realtime = useRealtime()
+
+    const counter = await realtime.client.counter.get();
+
+    await counter.getCount()
+
+    return (
+        <div></div>
+    )
 }
