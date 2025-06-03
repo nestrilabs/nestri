8f4bb05143b4e7458838a7e21c5c4eb07a2e94af
Wanjohi <elviswanjohi47@gmail.com>
2025-06-03 08:10:41 +0300

    🐜 fix(infra): Reduce DB ACUs

--- infra/postgres.ts
+++ infra/postgres.ts
@@ -1,16 +1,12 @@
 import { vpc } from "./vpc";
-import { isPermanentStage } from "./stage";
 
-// TODO: Add a dev db to use, this will help with running zero locally... and testing it
 export const postgres = new sst.aws.Aurora("Database", {
   vpc,
   engine: "postgres",
-  scaling: isPermanentStage
-    ? undefined
-    : {
-      min: "0 ACU",
-      max: "1 ACU",
-    },
+  scaling: {
+    min: "0 ACU",
+    max: "1 ACU",
+  },
   transform: {
     clusterParameterGroup: {
       parameters: [
